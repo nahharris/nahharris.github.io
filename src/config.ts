501d01facb848b42296ee3f073c34ae44d38d4af
Hannah Harrisonn <hannah@harrisonn.dev>
2026-01-08 20:27:30 -0300

    Update resume

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Harrisonn";
+export const SITE_TITLE = "Hannah Harrisonn";
 export const SITE_DESCRIPTION = "Welcome to my personal blog";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
-export const CONTACT_EMAIL = "harrisonn2004@gmail.com";
+export const CONTACT_EMAIL = "hannah@harrisonn.dev";
 export const SPECIAL_DATES = {
     6: {
         16: "🎉 Happy Birthday to me! 🎉"
